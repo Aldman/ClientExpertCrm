9c2ae6ad511343d3965945adc64abe5765a6a103
AlyoshkinDV <alyoshkin.dmitry@yandex.ru>
2025-10-29 21:06:26 +0300

    структура проектов с пустыми классами

---
+++ src/CRMService/Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace CRMService.Models;
+
+public class Client
+{
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Phone { get; set; }
+    public required Guid UserId { get; set; }
+    public required DateTime CreatedAt { get; set; }
+}
---
+++ src/CRMService/Models/Session.cs
@@ -0,0 +1,11 @@
+﻿namespace CRMService.Models;
+
+public class Session
+{
+    public required Guid Id { get; set; }
+    public required Guid ClientId { get; set; }
+    public required DateTime ScheduledAt { get; set; }
+    public required int DurationInMinutes { get; set; }
+    public required Status Status { get; set; }
+    public required string Notes { get; set; }
+}
